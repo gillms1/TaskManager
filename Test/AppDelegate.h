a31f6649eca9778815ac1beb42d27f4844f84aaa
Sunny <sunny@Sunnys-MacBook-Pro.local>
2015-06-28 19:36:47 +0200

    Initial Commit

---
+++ Test/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Test
+//
+//  Created by Sunny on 28/06/2015.
+//  Copyright (c) 2015 Sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Test/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Test
+//
+//  Created by Sunny on 28/06/2015.
+//  Copyright (c) 2015 Sunny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
